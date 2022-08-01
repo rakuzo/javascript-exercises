98d8a11a9b40aed337238821fae5a2943a7df5d5
rakuzo <rakuzoxilion@gmail.com>
2022-08-01 17:14:39 +0700

    Add my solution for test one

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(startArr, censor1, censor2) {
+    const initArr = startArr;
+        for (i = 0; i < initArr.length; i++) {
+            if (initArr[i] === censor1) {
+                initArr.splice(i,1);
+            }
+        }
+    return initArr;
 };
 
 // Do not edit below this line
