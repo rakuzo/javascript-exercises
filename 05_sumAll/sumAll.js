d4e184942eb50bce0245635a8f13d861b9789282
rakuzo <rakuzoxilion@gmail.com>
2022-08-01 20:55:25 +0700

    Add first solution to sumALL

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+    let total = 0;
+    for (i = start; i <= finish; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
