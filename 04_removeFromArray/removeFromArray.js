d29bef701ffd0826cff71b93673cdc56c01cb6b1
rakuzo <rakuzoxilion@gmail.com>
2022-08-01 20:39:59 +0700

    Fix removeFromArray using Rest parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,14 @@
-const removeFromArray = function(startArr, censor) {
-    const initArr = startArr;
-        for (i = 0; i < initArr.length; i++) {
-            if (initArr[i] === censor) {
+const removeFromArray = function(array, ...manyArgs) {
+    const initArr = array;
+    for (arg of manyArgs) {
+        for(let i = 0; i < array.length; i++) {
+            if (initArr[i] === arg) {
                 initArr.splice(i,1);
             }
         }
+    }
     return initArr;
-};
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
