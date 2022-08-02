2e562d31127d41ab58f96a31e14f6e7fb616fcdc
rakuzo <rakuzoxilion@gmail.com>
2022-08-02 13:51:06 +0700

    Add solution for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+    let celcResult = (fahr - 32) * (5 / 9);
+    return Math.round(celcResult * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celc) {
+    let fahrResult = celc * (9/5) + 32;
+    return Math.round(fahrResult * 10) / 10;
 };
 
 // Do not edit below this line
