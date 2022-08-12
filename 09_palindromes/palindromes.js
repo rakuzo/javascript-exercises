738d074d7a336692d978eb1c9126742e502d67b6
rakuzo <rakuzoxilion@gmail.com>
2022-08-12 20:30:24 +0700

    Remove console log
    Used it to debug the code.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -3,14 +3,12 @@ const reverseString = require("../03_reverseString/reverseString");
 const palindromes = function (words) {
     const wordsArray = Array.from(words.toUpperCase());
     const filtered = wordsArray.filter(item => /[a-zA-Z]/.test(item));
-    console.log(filtered);
     const reverseArray = [];
     for (let i = filtered.length; i >= 0; i--) {
         reverseArray.push(filtered[i])
     }
     const wordsString = filtered.join('');
     const revString = reverseArray.join('');
-    console.log(revString);
     return (wordsString === revString);
 };
 
