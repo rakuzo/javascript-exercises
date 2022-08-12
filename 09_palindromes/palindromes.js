21ea9cb80aaa1201c1c4856fc93479131f6987bf
rakuzo <rakuzoxilion@gmail.com>
2022-08-12 20:21:21 +0700

    Add my solution for palindrome 3rd test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const reverseString = require("../03_reverseString/reverseString");
 
+const palindromes = function (words) {
+    const wordsArray = Array.from(words.toUpperCase());
+    const filtered = wordsArray.filter(item => item !== '!');
+    console.log(filtered);
+    const reverseArray = [];
+    for (let i = filtered.length; i >= 0; i--) {
+        reverseArray.push(filtered[i])
+    }
+    const wordsString = filtered.join('');
+    const revString = reverseArray.join('');
+    console.log(revString);
+    return (wordsString === revString);
 };
 
 // Do not edit below this line
