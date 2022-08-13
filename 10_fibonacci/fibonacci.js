d4e7941b4e3ce3e26e29b50e3c2d7bbdf64f81ee
rakuzo <rakuzoxilion@gmail.com>
2022-08-13 11:57:23 +0700

    Add my solution for fibonachi

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {
+        return 'OOPS';
+    } else if (number <= 1) {
+        return parseInt(number);
+    } else {
+        return parseInt(fibonacci(number - 1) + fibonacci(number - 2));
+    }
 };
 
 // Do not edit below this line
