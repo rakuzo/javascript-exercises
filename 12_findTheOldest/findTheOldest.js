93ca513576f3ca3925db03608448960322dae62d
Dornell <rakuzoxilion@gmail.com>
2022-08-14 15:40:02 +0700

    Add my solution for find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(items) {
+    const currentDate = new Date().getFullYear();
+    const oldest = items.sort((a, b) => {
+        if (!('yearOfDeath' in a)) {
+            aLive = currentDate - a.yearOfBirth;
+            bLive = b.yearOfDeath - b.yearOfBirth;
+        } else if (!('yearOfDeath' in b)) {
+            aLive = a.yearOfDeath - a.yearOfBirth;
+            bLive = currentDate - b.yearOfBirth;
+        } else {
+            aLive = a.yearOfDeath - a.yearOfBirth;
+            bLive = b.yearOfDeath - b.yearOfBirth;
+        }
+        return (aLive > bLive) ? -1 : 1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
